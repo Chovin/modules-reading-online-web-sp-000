e301051b84ac32417d4343f998b53d7d771a59dc
Chovin Carlson <Chovin@users.noreply.github.com>
2019-03-09 10:42:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,7 +4,7 @@ require_relative './fancy_dance.rb'
 
 class Kid
   include FancyDance::InstanceMethods
-  include FancyDance::ClassMethods
+  extend FancyDance::ClassMethods
   
   attr_accessor :name
   
